7a0bb4740f14603dc4d1b9846a645c3deb24e3bb
Rudis Muiznieks <rmuiznieks@nextgxdx.com>
2017-03-26 13:17:56 -0500

    using sass for styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: `
-  <h1>Code Monkey Tycoon</h1>
-  <router-outlet></router-outlet>
-  `
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
